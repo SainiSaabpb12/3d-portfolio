8eb7519dad2f880f37b99831a552c3cd224c5ee1
SainiSaabpb12 <155221846+SainiSaabpb12@users.noreply.github.com>
2026-09-24 14:05:22 +0530

    Update portfolio content for current e-commerce experience

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -14,7 +14,7 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
-            <h3>E-commerce &</h3>
+            <h3>Marketplace, Shopify &</h3>
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Tech</div>
               <div className="landing-h2-2">Business</div>
